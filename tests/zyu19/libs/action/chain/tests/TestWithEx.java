fae4f68272ed9385274ff0cbbdfc13ecda51e82d
C4Phone <qq2900726136@gmail.com>
2015-08-23 15:35:02 -0500

    Add tests for Exception handling

---
+++ tests/zyu19/libs/action/chain/tests/TestWithEx.java
@@ -0,0 +1,183 @@
+package zyu19.libs.action.chain.tests;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import zyu19.libs.action.chain.ActionChain;
+import zyu19.libs.action.chain.config.Consumer;
+import zyu19.libs.action.chain.config.ErrorHolder;
+import zyu19.libs.action.chain.config.PureAction;
+import zyu19.libs.action.chain.config.ThreadChanger;
+import zyu19.libs.action.chain.config.ThreadPolicy;
+
+public class TestWithEx {
+	BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
+	ThreadPolicy threadPolicy = new ThreadPolicy(new ThreadChanger() {
+		@Override
+		public void runCallbackOnMainThread(Runnable runnable) {
+			queue.add(runnable);
+		}
+	}, Executors.newCachedThreadPool());
+	ActionChain chain = new ActionChain(threadPolicy);
+	
+	final Thread mainThread = Thread.currentThread();
+	
+	public boolean isMainThread() {
+		return Thread.currentThread() == mainThread;
+	}
+	
+	public Throwable getInnerMostEx(Throwable err) {
+		if(err == null)
+			return null;
+		
+		Throwable curr = err;
+		while(true) {
+			if(curr.getCause() == null)
+				return curr;
+			else curr = curr.getCause();
+		}
+	}
+	
+	@Test
+	public void TestExceptionCaughtAndProcessHalted() {
+		Random r = new Random();
+		final Exception err = new Exception(Double.toString(r.nextDouble())); 
+		final AtomicBoolean finished = new AtomicBoolean(false);
+		
+		new ActionChain(threadPolicy).fail(new Consumer<ErrorHolder>() {
+			public void consume(ErrorHolder arg) {
+				Assert.assertNotNull(arg);
+				Assert.assertNotNull(arg.getCause());
+				Assert.assertTrue("getCause() not correct.", arg.getCause().equals(err) || (getInnerMostEx(arg.getCause()) != null ? getInnerMostEx(arg.getCause()).equals(err) : false));
+				finished.set(true);
+			}
+		}).then(false, new PureAction<Object, Object>() {
+			public Object process(Object input) throws Exception {
+				throw new Exception(err);
+			}
+		}).then(true, new PureAction<Object, Object>() {
+			public Object process(Object input) throws Exception {
+				Assert.fail("ActionChain is not halted after exception is thrown (retry() not called)");
+				return null;
+			}
+		}).start(new Consumer<Object>() {
+			public void consume(Object arg) {
+				Assert.fail("onSuccess is run after exception is thrown (retry() not called)");
+				finished.set(true);
+			}
+		});
+		
+		// Simulate the Android Looper class
+		while (!finished.get())
+			try {
+				queue.take().run();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	@Test
+	public void TestRetry() {
+		final StringBuilder ansBuilder = new StringBuilder();
+		Random random = new Random();
+		
+		class TestOutputAction implements PureAction<Object, Object> {
+			public int name;
+			
+			public TestOutputAction(int name) {
+				this.name = name;
+			}
+			
+			public Object process(Object input) throws Exception {
+				ansBuilder.append(String.valueOf(name));
+				return null;
+			}
+		}
+		
+		// either RetryDecision or onSuccess should break the loop (see the while loop afterwards)
+		final AtomicBoolean finished = new AtomicBoolean(false);
+		
+		class RetryDecision implements Consumer<ErrorHolder> {
+			
+			public boolean willRetry;
+			
+			public RetryDecision(boolean willRetry) {this.willRetry = willRetry;}
+
+			@Override
+			public void consume(ErrorHolder arg) {
+				Assert.assertNotNull(arg);
+				if(willRetry)
+					arg.retry();
+				else finished.set(true);
+			}
+			
+		}
+		
+		PureAction<Object, Object> throwerAction = new PureAction<Object, Object>() {
+			boolean hasThrownOnce = false;
+			public Object process(Object input) throws Exception {
+				if(!hasThrownOnce) {
+					hasThrownOnce = true;
+					throw new Exception();
+				}
+				return null;
+			}
+		};
+		
+		chain.clear(new Consumer<ErrorHolder>() {
+			public void consume(ErrorHolder arg) {
+				Assert.fail(arg.getCause().toString());
+			}
+		});
+		
+		//----------------------------------
+		
+		String correctAns = "";
+		int temp;
+		
+		for (int i = 0; i < 100; i++) {
+			if(random.nextInt(10) >= 8) {
+				if(random.nextInt(10) >= 8) {
+					chain.fail(new RetryDecision(false));
+					chain.then(random.nextBoolean(), throwerAction);
+					break;
+				}
+				else {
+					chain.fail(new RetryDecision(true));
+					chain.then(random.nextBoolean(), throwerAction);
+				}
+			} else {
+				temp = random.nextInt();
+				correctAns += String.valueOf(temp);
+				chain.then(random.nextBoolean(), new TestOutputAction(temp));
+			}
+		}
+		
+		final int lastTest = random.nextInt();
+		correctAns += String.valueOf(lastTest);
+		
+		chain.start(new Consumer<Object>() {
+			public void consume(Object arg) {
+				ansBuilder.append(String.valueOf(lastTest));
+				finished.set(true);
+			}
+		});
+		
+		// Simulate the Android Looper class
+		while (!finished.get())
+			try {
+				queue.take().run();
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		
+		Assert.assertTrue(ansBuilder.toString() + " != " + correctAns, ansBuilder.toString().equals(correctAns));
+	}
+}
